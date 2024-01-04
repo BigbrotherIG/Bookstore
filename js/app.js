36368b62a44c4a6a784a2ee20a922556e767eef6
BigbrotherIG <itzbigbroda@gmail.com>
2024-01-04 13:17:29 +0100

    Added GITHUB API call

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+    mainHeader = document.querySelector(".main-header")
+    window.addEventListener("scroll", (e) => {
+        
+        if(scrollY >= 160){
+            console.log(e)
+        }
+    })
